76e194cac91bffdf1fa581f85db27f1332f7f685
Cameron Daigle <camerondaigle@gmail.com>
2015-02-26 14:00:55 -0500

    Finish up basic ui

---
+++ app/components/ui-subnav.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'div',
+  classNames: ['subnav'],
+  classNameBindings: ['open'],
+  open: false,
+  actions: {
+    toggle: function() {
+      this.toggleProperty('open');
+    }
+  }
+});
--- config/environment.js
+++ config/environment.js
@@ -18,7 +18,8 @@ module.exports = function(environment) {
       // when it is created
     },
     sassOptions: {
-      inputFile: 'app.sass'
+      inputFile: 'app.sass',
+      includePaths: ['vendor/styles']
     }
   };
 
